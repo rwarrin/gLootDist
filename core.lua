57affdda5591ac4599842901b6b49ff63952b01a
Rick Warrington <phial1020@gmail.com>
2011-01-30 11:05:16 -0700

    Rewrote CommandHandler() function and updated OnUpdate() function.
    I added a check to see if the player is a raid leader or officer to
    check whether or not the loot roll announcement should be displayed
    in raid chat or as a raid warning.
    
    I added a new variable countdown because COUNT_DOWN_TIMER was being
    used as the counter and was not working as a constant configuration
    variable.
    
    Completely rewrote the CommandHandler() function and now it works
    with "help" or no arguments at all.

--- core.lua
+++ core.lua
@@ -7,7 +7,8 @@ local lastupdate = 0;
 local updatefrequency = 2;
 local inprogress = false;
 local currentitemlink = nil;
-local rolltype = "need";
+local rolltype = nil;
+local countdown = 0;
 
 -- Event handling function
 local function OnEvent(self, event, ...)
@@ -21,21 +22,24 @@ end
 local function OnUpdate(self, elapsed)
     lastupdate = lastupdate + elapsed;
     
-    if(lastupdate >= updatefrequency) then  
-        if(inprogress == true) then
-            if(COUNT_DOWN_TIMER == 5) then
-                SendChatMessage(string.upper(rolltype) .. " " .. currentitemlink, "RAID_WARNING", "COMMON");
-				--print(string.upper(rolltype) .. " " .. currentitemlink);
+	if(inprogress == true) then
+		if(lastupdate >= updatefrequency) then  
+            if(countdown == 5) then
+				if(IsRaidLeader() == 1 or IsRaidOfficer() == 1) then
+					SendChatMessage(string.upper(rolltype) .. " " .. currentitemlink, "RAID_WARNING", "COMMON");
+				else
+					SendChatMessage(string.upper(rolltype) .. " " .. currentitemlink, "RAID", "COMMON");
+				end
             end
 
-			SendChatMessage(COUNT_DOWN_TIMER .. ". . .", "RAID", "COMMON");
+			SendChatMessage(countdown .. ". . .", "RAID", "COMMON");
 			
-            if(COUNT_DOWN_TIMER <= 0) then
+            if(countdown <= 0) then
                 inprogress = false;
             end
             
             lastupdate = 0;
-            COUNT_DOWN_TIMER = COUNT_DOWN_TIMER - 1;
+            countdown = countdown - 1;
         end
     end
 end
@@ -45,21 +49,18 @@ glootframe:RegisterEvent("PLAYER_ENTERING_WORLD");
 glootframe:SetScript("OnEvent", OnEvent);
 glootframe:SetScript("OnUpdate", OnUpdate);
 
--- Function to handle arguments from the slash command
+-- Function to handle slash command
 local function CommandHandler(msg)
-    if(msg ~= nil) then
-        local rtype, item = msg:match("^(%S*)%s*(.-)$");
-		if(rtype == nil or item == nil) then
-			print("gLootDist: Usage:  /gloot <rolltype> <item link>");
-		else
-			local itemName, itemLink, itemRarity, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount, itemEquipLoc, itemTexture, itemSellPrice = GetItemInfo(item);
-			currentitemlink = itemLink;
-			inprogress = true;
-			COUNT_DOWN_TIMER = 5;
-			rolltype = rtype;
-		end
-    else
-		print("gLootDist: Usage:  /gloot <rolltype> <item link>");
+	local rtype, item = msg:match("^(%S*)%s*(.-)$");
+	
+	if(rtype == "" or rtype == "help") then
+		print("|cfffff000gLootDist:|r Usage:  /gloot |cffff2b4e<rolltype>|r |cffa335ee<item link>|r");
+	else
+		local itemName, itemLink, itemRarity, itemLevel, itemMinLevel, itemType, itemSubType, itemStackCount, itemEquipLoc, itemTexture, itemSellPrice = GetItemInfo(item);
+		rolltype = rtype;
+		currentitemlink = itemLink;
+		inprogress = true;
+		countdown = COUNT_DOWN_TIMER;
 	end
 end
 
